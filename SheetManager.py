7cf9ad2686509e04e5305cbe93d3f481acdfea04
Unkqwn <unlqwn@hotmail.com>
2026-01-21 18:14:12 +0100

    Getting presets and adding languages.

--- SheetManager.py
+++ SheetManager.py
@@ -1,6 +1,17 @@
 import json
 from pathlib import Path
 
+language = "en"
+
+stringPath = (
+    Path(__file__).resolve().parent
+    / "Strings"
+    / f"{language}.json"
+)
+
+with stringPath.open(encoding="utf-8") as file:
+    text = json.load(file)
+
 presetFolder = Path(__file__).resolve().parent / "Presets"
 preset = None
 
@@ -9,25 +20,62 @@ def GetFiles(folder: Path):
     files = [f for f in folder.iterdir() if f.is_file()]
     return files
 
-def CheckFolder():
-    if not presetFolder.exists():
-        print("No folder found. Please check if you downloaded the presets along with this code.")
-        return False
+def CheckFolder(folder):
+    if not folder.exists():
+        print(text["noFolder"])
+        return None
     
-    files = GetFiles(presetFolder)
+    files = GetFiles(folder)
     if not files:
-        print("No files were found in the folder. Please check if you downloaded the files along with the folder.")
-        return False
+        print(text["noFiles"])
+        return None
     
-    return True
+    return files
 
-def ChoosePreset():
+def ChoosePreset(presets: list[Path]):
     """Placeholder for when I'll let the user select the preset."""
-    return
+    print(text["availablePresets"])
+
+    for index, preset_file in enumerate(presets, start = 1):
+        print(f"{index}. {preset_file.stem}")
+
+    print(text["choice"])
+
+    choice = input("Choice: ").strip()
+
+    return choice
+
+def HandleChoice(choice, presets: list[Path]):
+    if not choice.isdigit():
+        print(text["invalidChoice"])
+        return None
+
+    index = int(choice) - 1
+
+    if index < 0 or index >= len(presets):
+        print(text["invalidChoice"])
+        return None
+
+    return presets[index]
+
+def CreateSheet():
+    pass
+
+def CalculateModifier(score: int) -> int:
+    return (score - 10) // 2
 
 def Main():
-    if CheckFolder():
-        print("Preset files were found.")
+    print(text["welcome"])
+
+    presets = CheckFolder(presetFolder)
+    if not presets:
+        return
+
+    preset = HandleChoice(ChoosePreset(presets), presets)
+    if not preset:
+        return
+
+    print(preset.stem)
     return
 
 if __name__ == "__main__":
